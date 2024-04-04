57d3feeffdebfb28c78134c3c8fe0284dc40b0e8
dilbot-cot <131760791+dilbot-cot@users.noreply.github.com>
2024-04-04 20:12:35 +1100

    Half way of class

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
-console.log("Hello from the script.js file!")
+// // let is a local scope
+// let exampleEmptyVariable = null;
+// let anotherEmptyVariable = undefined;
+
+// console.log(exampleEmptyVariable, anotherEmptyVariable);
+
+// let message = "hello from the script.js file!";
+
+// console.log(message);
+// console.log(message * 2);
+
+// let world = "Earth";
+
+// console.log(`Hello ${world}, I am Dillon!`); // This is sting interperlation
+
+// console.log("Hello " + world + ", I am Dillon!");
+
+// {
+//     // for this scope world changes to Mars, but before and after this it will remain as Earth
+//     let world = "Mars";
+//     console.log(`Hello ${world}, I am Dillon!`); // This is sting interperlation
+// }
+
+// console.log(`Hello ${world}, I am Dillon!`); 
+
+// Array in JS
+let favouriteFruits = [
+    "Mango",
+    "Banana",
+    "Grapes",
+    "Passionfruit",
+    "Pear",
+    "Strawberry",
+    "Watermelon",
+    "Lychee",
+    "Cherry",
+    "Blueberry",
+    "Tomato",
+    "Papaya",
+    "Dragonfruit",
+    "Kiwi"
+];
+
+console.log(favouriteFruits[12]);
+
+// favouriteFruits.forEach(fruit => {
+//     console.log(`Somone in the class loves ${fruit}`);
+// })
+
+// for (fruitIndex in favouriteFruits) {
+//     console.log(`Someone in the class loves ${favouriteFruits[fruitIndex]}`)
+// }
+
+console.log(`The class' favourite fruits are ${favouriteFruits}`);
+
+let sortedFavouriteFruits = favouriteFruits.sort()
+
+console.log(sortedFavouriteFruits)
